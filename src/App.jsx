b6c61e268c34b0ef4702e27abacbbfb16d31831e
Igor Ivanov <igor.ivanov.89@gmail.com>
2025-05-28 19:21:30 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -158,11 +158,16 @@ export default function App() {
             ]} /> */}
             <ListItem title="Наліт та типи ПС">
               <ul className="list-disc pl-6 text-sm text-gray-300 space-y-1">
+                <li className="!list-none mt-2">
+                  <span className="text-sm italic text-gray-400">
+                    Льотна підготовка
+                  </span>
+                </li>
                 <li>Diamond DA42 — 50 год 18 хв</li>
                 <li>Tecnam P2002-JF — 1 год 00 хв</li>
                 <li>Aquila AT01 — 66 год 30 хв</li>
                 <li>НАРП-1 — 40 год 03 хв</li>
-                <li className="!list-none mt-2">
+                <li className="!list-none">
                   <span className="text-sm italic text-gray-400">
                     Тренажерна підготовка
                   </span>
